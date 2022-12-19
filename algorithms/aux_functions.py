b047442e994a88e0372e7b702b1149a84bb6d5cd
ECGomes <eduardo.c.gomes@tecnico.ulisboa.pt>
2022-12-19 20:38:05 +0000

    Optimized window splitting

--- algorithms/aux_functions.py
+++ algorithms/aux_functions.py
@@ -1,23 +1,11 @@
 import numpy as np
 
-# Adapted from the machinelearningmastery website:
-# https://machinelearningmastery.com/time-series-prediction-lstm-recurrent-neural-networks-python-keras/
-def split_sequence(sequence, n_steps, n_intervals=1):
-    X = list()
-    for i in np.arange(0, len(sequence), n_intervals):
-        # find the end of this pattern
-        end_ix = i + n_steps
-        # check if we are beyond the sequence
-        if end_ix > len(sequence) - 1:
-            break
-        # gather input and output parts of the pattern
-        seq_x = sequence[i:end_ix]
-        X.append(seq_x)
-    return np.array(X)
 
+def split_sequence(sequence, n_steps):
+    sequence = np.asarray(sequence)
+    n_windows = len(sequence) - n_steps
+    windows = np.empty((n_windows, n_steps), dtype=sequence.dtype)
+    windows[:, :] = sequence[:-n_steps][:, None]
+    labels = sequence[n_steps:]
+    return windows, labels
 
-def create_windows(sequence, n_steps):
-    X = [[sequence[i + j] for j in range(n_steps)] for i in range(len(sequence) - n_steps)]
-    y = [sequence[i + n_steps] for i in range(len(sequence) - n_steps)]
-
-    return X, y
